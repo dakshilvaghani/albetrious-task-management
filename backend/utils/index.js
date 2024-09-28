034449c024a3ab62677a846b7b53b102c316d8f8
Vaghani Dakshil <119387136+dakshilvaghani@users.noreply.github.com>
2024-09-28 11:39:39 +0530

    Update index.js

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -11,29 +11,3 @@ const dbConnection = async () => {
 };
 
 export default dbConnection;
-
-export const createJWT = async (res, user) => {
-  console.log(user);
-  try {
-    const payload = {
-      userid: user._id,
-      email: user.email,
-      username: user.username,
-    };
-
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: "15d",
-    });
-    console.log("inside create jwt and token is", token);
-    const options = {
-      expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-    };
-    res.cookie("token", token, options);
-    res.set("Authorization", `Bearer ${token}`);
-  } catch (error) {
-    console.error("Error generating JWT token:", error);
-  }
-};
